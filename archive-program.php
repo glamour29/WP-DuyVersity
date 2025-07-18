c203fa14e5cb10ab5748543a9cdb2e9ab8d38a41
glamour29 <duylt0022@ut.edu.vn>
2025-07-18 15:15:10 +0700

    Push partical WP project

---
+++ archive-program.php
@@ -0,0 +1,26 @@
+<?php
+get_header(); // Gọi header của theme
+pageBanner(array(
+    'title' => 'All Programs',
+    'subtitle' => 'There is something for everyone. Have a look around!',
+));
+?>
+
+<div class="container container--narrow page-section">
+
+  <ul class="link-list min-list">    
+    <?php
+    while (have_posts()) {
+      the_post(); ?>
+      <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+    <?php }
+    ?>
+  </ul>
+
+  <?php echo paginate_links(); ?>
+
+  <hr class="section-break">
+  <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events'); ?>">Check out our past events archive.</a></p>
+</div>
+
+<?php get_footer(); ?>
---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery'), 'version' => 'aaa87451a316ab574c80');
